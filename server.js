8aa0630ed8f725a728e006f68e0fdc5a36dc9cf0
AshMohanty <mohantay@gmail.com>
2024-05-20 22:53:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 const {
     VGS_VAULT_ID,
